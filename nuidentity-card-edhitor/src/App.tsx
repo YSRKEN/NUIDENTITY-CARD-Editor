e8240faf9ff7e83322ed1658de08fa03798caaa0
YSRKEN <ysr.ken@gmail.com>
2020-11-27 01:30:31 +0900

    [add] Bootstrapを導入した

--- nuidentity-card-edhitor/src/App.tsx
+++ nuidentity-card-edhitor/src/App.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 
-const App: React.FC = () => <h1>NUIDENTITY CARD EDITOR</h1>;
+const App: React.FC = () => (
+  <Container>
+    <Row className="my-3">
+      <Col className="text-center">
+        <h1>NUIDENTITY CARD EDITOR</h1>
+      </Col>
+    </Row>
+  </Container>
+);
 
 export default App;
