849976f910c8cde781cee38c5c5509a772952bb1
YSRKEN <ysr.ken@gmail.com>
2020-11-27 12:53:16 +0900

    [add] 画像を読み込めることを確認した

--- nuidentity-card-edhitor/src/container/MainForm.tsx
+++ nuidentity-card-edhitor/src/container/MainForm.tsx
@@ -1,15 +1,29 @@
 import React, { useContext } from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import { Image, Layer, Stage } from "react-konva";
 import { ApplicationContext } from "service/store";
+import card86 from 'asset/nuid_temple_86.png';
+import useImage from "use-image";
 
 const MainForm: React.FC = () => {
   const { dispatch } = useContext(ApplicationContext);
+
+  const [imageData] = useImage(card86);
 
   return (
     <Container>
       <Row className="my-3">
         <Col className="text-center">
           <h1 onClick={() => dispatch({ type: '' })}>ぬいぐるみ用名刺エディタ</h1>
+        </Col>
+      </Row>
+      <Row className="my-3">
+        <Col>
+          <Stage width={1076} height={650}>
+            <Layer>
+              <Image image={imageData} x={0} y={0} width={1076} height={650} />
+            </Layer>
+          </Stage>
         </Col>
       </Row>
     </Container>
